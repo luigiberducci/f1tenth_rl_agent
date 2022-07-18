ab21f4dbbb9dd2772533d0d0887fb9bb3557f1c3
luigiberducci <luigi.berd@gmail.com>
2022-07-18 18:00:40 +0200

    adding a fundamental equal sign

--- rl_node/docker_starter.sh
+++ rl_node/docker_starter.sh
@@ -5,6 +5,6 @@ IMAGE_NAME="rl_node:latest"
 PY_CMD="python3 /rl_node/src/node.py"
 NODE_DIR=$(rospack find rl_node)
 
-CMD="docker run -it --rm --net host -v $NODE_DIR:/rl_node $IMAGE_NAME $PY_CMD"
+CMD="docker run -it --rm --net=host -v $NODE_DIR:/rl_node $IMAGE_NAME $PY_CMD"
 echo $CMD
 $CMD
